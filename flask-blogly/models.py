39bf7d4a782b56c91ba03775e7bd7297ebffca22
Thitiat Pidtatasang <Thitiat1230@gmail.com>
2022-04-06 03:36:55 -0700

    Add files via upload

---
+++ flask-blogly/models.py
@@ -0,0 +1,29 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+
+class User(db.model):
+    ___tablename__ = 'user'
+
+    id = db.Column(db.integer, primary_key = True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+
+def conncet_db(app):
+    db.app = app
+    db.init_app(app)
+
+    
+
